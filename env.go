564ee1e53f5d492df04509a2b8e9bc4b778f9cea
William Burton <willthedeveloper13@gmail.com>
2022-04-03 18:17:04 +0100

    Set the port for the server

--- env.go
+++ env.go
@@ -6,7 +6,7 @@ import (
 
 // Set the environment variables
 func SetEnvironment() {
-	os.Setenv("port", "")          //PORT OF THE SERVER
+	os.Setenv("port", "8080")      //PORT OF THE SERVER
 	os.Setenv("host", "localhost") //HOST ADDRESS OF THE SERVER
 }
 
