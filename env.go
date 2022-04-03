9cc01f4173d2aeaaf05279c42a716e5622d78667
William Burton <willthedeveloper13@gmail.com>
2022-04-03 18:12:06 +0100

    Added environment checks

--- env.go
+++ env.go
@@ -6,6 +6,18 @@ import (
 
 // Set the environment variables
 func SetEnvironment() {
-	os.Setenv("port", "8080")      //PORT OF THE SERVER
+	os.Setenv("port", "")          //PORT OF THE SERVER
 	os.Setenv("host", "localhost") //HOST ADDRESS OF THE SERVER
+}
+
+// Checks that the environment has been setup correctly
+func CheckEnvironment() {
+	if os.Getenv("port") == "" {
+		println("Server port has not been set.")
+		os.Exit(69)
+	}
+	if os.Getenv("host") == "" {
+		println("Server host has not been set.")
+		os.Exit(70)
+	}
 }
--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) { // This is what is displayed on the page
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-	SetEnvironment()
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("port"), nil))
+	CheckEnvironment()                                         // Check the environment variables are set correctly.
+	SetEnvironment()                                           // Apply the environment variables.
+	println("Server started on:" + os.Getenv("port"))          // Output
+	http.HandleFunc("/", handler)                              // Serve requests from root
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("port"), nil)) // Begin serving requests
 }
